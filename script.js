0c279032d24862a2871906b7eb315985093fd3d3
Sai Narendra <tannirusainarendra81@gmail.com>
2025-08-02 18:34:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-//your JS code here. If required.
 const squares = document.querySelectorAll(".square");
 
 squares.forEach((square, index) => {
-	square.addEventListener("mouseenter", () => {
+	square.addEventListener("mouseover", () => {
 		squares.forEach((otherSquare, otherIndex) => {
 			if (otherIndex !== index) {
 				otherSquare.style.backgroundColor = "#6F4E37"; // Coffee color
@@ -10,7 +9,7 @@ squares.forEach((square, index) => {
 		});
 	});
 
-	square.addEventListener("mouseleave", () => {
+	square.addEventListener("mouseout", () => {
 		squares.forEach(sq => {
 			sq.style.backgroundColor = "#E6E6FA"; // Lavender color
 		});
